47fd14340f51e358d6d13913a9ddb90d597acf4f
djfj1 <djfj1@gcloud.ua.es>
2024-03-22 12:24:26 +0100

    53979820Q. Añadido pagina master y configurando la conexión.

--- library/CADProduct.cs
+++ library/CADProduct.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Configuration;
 
 namespace library
 {
     public class CADProduct
     {
+        static string cadena = ConfigurationManager.ConnectionStrings["miconexion"].ToString();
+        SqlConnection c = new SqlConnection(cadena);
+    }
+
+    public bool Create(ENProduct en)
+    {
+        
+        return true;
     }
 }
