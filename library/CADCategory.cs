1e6017b9fd180bf2e89fa373b8f6f6fc76e2bd3e
Ergin Haxhijaj <eh54@alu.ua.es>
2024-03-25 10:21:18 +0100

    X7306964W Se han añadido los métodos de CADCategory (aún falta ver qué hacer con la categoría leída en Read()

--- library/CADCategory.cs
+++ library/CADCategory.cs
@@ -11,5 +11,71 @@ namespace library
 {
     public class CADCategory
     {
+        public string constring { get; set; }
+
+        public CADCategory()
+        {
+            constring = ConfigurationManager.ConnectionStrings["miconexion"].ToString();
+        }
+
+        // Devuelve la categoría en la base de datos
+        public bool Read(ENCategory en)
+        {
+            bool retValue;
+
+            SqlConnection c = new SqlConnection(constring);
+            try
+            {
+                c.Open();
+                SqlCommand com = new SqlCommand("select * from categoria where id = " + en.id + ");", c);
+                SqlDataReader dr = com.ExecuteReader();
+
+                // falta ver qué hacer con la fila leída
+                dr.Read();
+                dr.Close();
+                retValue = true;
+            }
+            catch
+            {
+                retValue = false;
+            }
+            finally
+            {
+                c.Close();
+            }
+
+            return retValue;
+        }
+
+        // Devuelve todas las categorías de la base de datos
+        public List<ENCategory> readAll()
+        {
+            List<ENCategory> categories = new List<ENCategory>();
+
+            SqlConnection c = new SqlConnection(constring);
+            try
+            {
+                c.Open();
+                SqlCommand com = new SqlCommand("select * from categoria;", c);
+                SqlDataReader dr = com.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    categories.Add(new ENCategory(dr["id"], dr["name"]));
+                }
+
+                dr.Close();
+            }
+            catch
+            {
+                
+            }
+            finally
+            {
+                c.Close();
+            }
+
+            return categories;
+        }
     }
 }
